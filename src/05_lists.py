1255cc72e58e9973efc1261b00c107d4b5e53093
Jesse-Tingle <jtingle0@gmail.com>
2020-08-06 19:43:21 -0400

    makes some corrections to formating printing.py

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,42 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+
+# x.append(4)
+# print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+# x = x + y
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+
+# x.append(4)
+# x = x + y
+# for num in x:
+#     if num == 8:
+#         x.remove(num)
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+
+# x.append(4)
+# x = x + y
+# for num in x:
+#     if num == 8:
+#         x.remove(num)
+
+# x.insert(5, 99)
+
+# print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+# print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
@@ -33,7 +53,7 @@ z = [
     [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
     [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
     [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-    [40 ,41, 42, 43, 44, 45, 46, 47, 48, 49],
+    [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
     [50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
     [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
     [70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
@@ -41,9 +61,18 @@ z = [
     [90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
 ]
 
+# num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+my_new_list = []
+for i in z:
+    print(i * 1000)
+    for n in i:
+        print(n * 1000)
+
+
 # Print the first element from each nested list in the matrix z
 # YOUR CODE HERE
 
-# Print all of the elements that lie on the left-to-right 
-# diagonal of matrix z 
+
+# Print all of the elements that lie on the left-to-right
+# diagonal of matrix z
 # YOUR CODE HERE
